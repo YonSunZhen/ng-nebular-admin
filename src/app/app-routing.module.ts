9d2afa9f09c532415e47709ef8f40775c66ec34f
YonSunZhen <1315976441@qq.com>
2019-08-27 15:14:52 +0800

    添加auth模块页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+import {
+  NbAuthComponent,
+  NbLoginComponent,
+  NbLogoutComponent,
+  NbRegisterComponent,
+  NbRequestPasswordComponent,
+  NbResetPasswordComponent,
+} from '@nebular/auth';
 
 
 const routes: Routes = [
@@ -7,6 +15,36 @@ const routes: Routes = [
     path: 'pages',
     loadChildren: () => import('./pages/pages.module')
       .then(m => m.PagesModule),
+  },
+  {
+    path: 'auth',
+    component: NbAuthComponent,
+    children: [
+      {
+        path: '',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'login',
+        component: NbLoginComponent,
+      },
+      {
+        path: 'register',
+        component: NbRegisterComponent,
+      },
+      {
+        path: 'logout',
+        component: NbLogoutComponent,
+      },
+      {
+        path: 'request-password',
+        component: NbRequestPasswordComponent,
+      },
+      {
+        path: 'reset-password',
+        component: NbResetPasswordComponent,
+      },
+    ],
   },
   { path: '', redirectTo: 'pages', pathMatch: 'full' },
   { path: '**', redirectTo: 'pages' } // **表示通配符
--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -24,5 +24,27 @@ export const MENU_ITEMS: NbMenuItem[] = [
                 link: '/pages/layout/list',
             },
         ],
+    },
+    {
+        title: 'Auth',
+        icon: 'lock-outline',
+        children: [
+            {
+                title: 'Login',
+                link: '/auth/login',
+            },
+            {
+                title: 'Register',
+                link: '/auth/register',
+            },
+            {
+                title: 'Request Password',
+                link: '/auth/request-password',
+            },
+            {
+                title: 'Reset Password',
+                link: '/auth/reset-password',
+            },
+        ],
     }
 ];
